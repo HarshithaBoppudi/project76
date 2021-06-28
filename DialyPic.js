85afda7ff36275b158f18a3b46d05ba7fde4f7b9
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2021-06-28 20:22:32 +0530

    Add files via upload

--- DialyPic.js
+++ DialyPic.js
@@ -1,14 +1,113 @@
 import * as React from 'react';
-import { View, StyleSheet, Button,TextInput,Text,TouchableOpacity,Image } from 'react-native';
+import { View, StyleSheet, Button,TextInput,Text,TouchableOpacity,Image,StatusBar,FlatList,SafeAreaView,ImageBackground,
+Dimensions,Platform} from 'react-native';
+import axios from 'axios';
+import { Linking } from 'react-native';
 
 
-export default class DailyPicScreen extends React.Component {
-render(){
+export default class MeteorsScreen extends React.Component {
+  constructor(){
+    super()
+    this.state={
+     apod:{}
+    }
+  }
+  getAPOD=()=>{
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=jAla0SLbzblS7tWGt2r7Q0EKHTgig1YhnqamTCMN')
+    .then(response=>{
+      this.setState({
+        apod:response.data
+      })
+    })
+    .catch(error=>{
+      alert(error.message)
+    })
+  }
+  componentDidMount(){
+    this.getAPOD()
+  }
+
+  renderItem=({item})=>{
+return(
+  <View style={styles.container}>
+    <SafeAreaView style={styles.droidsafearea}/>
+    <ImageBackground source={require('../assets/img.jpg')} style={styles.backgroundImg}>
+  <Text style={styles.routeText}>Astronomy picture of the day</Text>
+  <Text style={styles.titleText}>{this.state.apod.title}</Text> 
+  <TouchableOpacity style={styles.listContainer}
+  onPress={()=> Linking.openURL(this.state.apod.url).catch(err=> console.error('could not load page',err))}
+  >
+     
+  </TouchableOpacity>
+  <TouchableOpacity>
+    <Text style={styles.explanationText}>{this.state.apod.explanation}</Text>
+  </TouchableOpacity>
+    </ImageBackground>
+
+  </View>
+)
+
+  }
+  keyExtractor=(item,index)=>{
+    index.toString()
+  }
+  
+  render(){
+   let apod=this.state.apod
   return(
-    <View>
-    <Text>daily pics Screen</Text>
+    <View style={{flex:1}}>
+      <SafeAreaView style={styles.droidsafearea}/>
+      <FlatList
+      keyExtractor={this.keyExtractor}
+      data={apod}
+      renderItem={this.renderItem}  
+      horizontal={true}
+      />
     </View>
   )
-}
+  }
 
-}
+
+  
+  }
+  const styles=StyleSheet.create({
+    droidsafearea:{
+      marginTop:Platform.OS==='android'?StatusBar.currentHeight:0
+    },
+    container:{
+     flex:1,
+     justifyContent:'center',
+      alignItems:'center'
+    },
+    backgroundImg:{
+      flex:1,
+      resizeMode:'cover',
+      marginBottom:0,
+      height:690
+    },
+    titleText:{
+      fontSize:30,
+      fontWeight:'bold',
+      color:'white',
+      alignItems:'center',
+      justifyContent:'center'
+    },
+    
+    routeText:{
+      fontSize:'10',
+      fontWeight:'bold',
+      color:'white',
+      justifyContent:'center'
+    },
+    listContainer:{
+      justifyContent:'center',
+      margin:10,
+      borderRadius:10,
+      padding:10
+    },
+    explanationText:{
+      fontSize:15,
+      justifyContent:'center',
+      alignItems:'center'
+    }
+  })
